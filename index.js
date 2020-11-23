09c3a805583b65692e3c9292ffff7029de70d5d3
Austin Bell <snarkeet@gmail.com>
2020-11-23 00:23:35 -0500

    converted data to GeoJSON

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const map = L.map("map").setView([54.5260, -105.2551], 2.5);
+const map = L.map("map").setView([37.0902, -95.7129], 4);
+let features = [];
 
 L.tileLayer(
   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoieGF2aWVyYmVsbGhhZGRvbiIsImEiOiJja2h0dWJzd3owMnV0MnJydmI5dXp1MjJrIn0.XsZhwZnA3zq2_SZZ5TF1RA",
@@ -6,7 +7,7 @@ L.tileLayer(
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: "mapbox/light-v10",
+    id: "mapbox/dark-v10",
     tileSize: 512,
     zoomOffset: -1,
     accessToken:
@@ -14,23 +15,42 @@ L.tileLayer(
   }
 ).addTo(map);
 
-const style = {
-    "color": "#83d71e",
-    "weight": 2,
-    "opacity": 1,
-    "fillOpacity": 0.65
-}
+// const style = {
+//     "color": "#83d71e",
+//     "weight": 2,
+//     "opacity": 1,
+//     "dashArray": 5,
+//     "fillOpacity": 0.65
+// }
 
-L.geoJson(statesData, {
-    style: style
-}).addTo(map);
+// L.geoJson(statesData, {
+//     style: style
+// }).addTo(map);
 
 let url = "https://api.846policebrutality.com/api/incidents";
 const req = new XMLHttpRequest();
 req.open("GET", url);
 req.send();
 req.onload = function () {
-  const json = JSON.parse(req.responseText);
-  console.log(json);
+    
+  const data = JSON.parse(req.responseText).data;
+
+  data.forEach(incident => {
+      const lat = incident.geocoding.lat;
+      const long = incident.geocoding.long;
+
+      const feature = {type: 'Feature',
+      properties: incident,
+      geometry: {
+          type: 'Point',
+          coordinates: [long, lat]
+      }
+    };
+
+    features.push(feature)
+
+  })
 };
+
+console.log(features)
 
