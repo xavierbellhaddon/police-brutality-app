d838511631866870faac2dab1287ca954dd671e5
Austin Bell <snarkeet@gmail.com>
2020-11-24 18:08:35 -0500

    list search results

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const form = document.querySelector("form");
 const textInput = document.querySelector("#text-input");
-const map = L.map("map").setView([37.0902, -95.7129], 3);
+const map = L.map("map", {
+  scrollWheelZoom: false,
+}).setView([37.0902, -95.7129], 4);
+const searchResults = document.querySelector("#search-results");
 const style = {
-    "color": "white",
-    "fillColor": "black",
-    "weight": 1,
-    "opacity": 0.25,
-    "dashArray": 0,
-    "fillOpacity": 0.65
-}
-const accessToken = "pk.eyJ1IjoieGF2aWVyYmVsbGhhZGRvbiIsImEiOiJja2h0dWJzd3owMnV0MnJydmI5dXp1MjJrIn0.XsZhwZnA3zq2_SZZ5TF1RA"
+  color: "white",
+  fillColor: "black",
+  weight: 1,
+  opacity: 0.25,
+  dashArray: 0,
+  fillOpacity: 0.65,
+};
+const accessToken = "pk.eyJ1IjoieGF2aWVyYmVsbGhhZGRvbiIsImEiOiJja2h0dWJzd3owMnV0MnJydmI5dXp1MjJrIn0.XsZhwZnA3zq2_SZZ5TF1RA";
 
 L.tileLayer(
-  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=" + accessToken,
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=" +
+    accessToken,
   {
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -20,13 +24,12 @@ L.tileLayer(
     id: "mapbox/dark-v10",
     tileSize: 512,
     zoomOffset: -1,
-    accessToken:
-      accessToken,
+    accessToken: accessToken,
   }
 ).addTo(map);
 
 L.geoJson(statesData, {
-    style: style
+  style: style,
 }).addTo(map);
 
 function visualize() {
@@ -36,34 +39,45 @@ function visualize() {
   req.send();
   req.onload = function () {
     const data = JSON.parse(req.responseText).data;
-    data.forEach(incident => {
-        const lat = incident.geocoding.lat;
-        const long = incident.geocoding.long;
-        L.circle([lat, long], {
-          color: 'red',
-          fillColor: '#f03',
-          fillOpacity: 0.05,
-          radius: 50000,
-          weight: 1
+    data.forEach((incident) => {
+      const lat = incident.geocoding.lat;
+      const long = incident.geocoding.long;
+      L.circle([lat, long], {
+        color: "red",
+        fillColor: "#f03",
+        fillOpacity: 0.05,
+        radius: 50000,
+        weight: 1,
       }).addTo(map);
-    })
+    });
   };
 }
 
 function handleSearch(searchTerm) {
-  let url = "https://api.846policebrutality.com/api/incidents?filter[state]=" + searchTerm.split(" ").join("+");
+  let url =
+    "https://api.846policebrutality.com/api/incidents?filter[state]=" +
+    searchTerm.split(" ").join("+");
   const req = new XMLHttpRequest();
   req.open("GET", url);
   req.send();
   req.onload = function () {
     const data = JSON.parse(req.responseText).data;
-    console.log(data)
-  }
+    console.log(data);
+    data.forEach((incident) => {
+      const el = document.createElement("div");
+      el.innerHTML = `
+      <h2>${incident.title}</h2>
+      <p>${incident.date}</p>
+      `;
+      searchResults.appendChild(el);
+    });
+  };
 }
 
 visualize();
 
-form.addEventListener("submit", event => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
+  searchResults.innerHTML = "";
   handleSearch(textInput.value);
-})
+});
