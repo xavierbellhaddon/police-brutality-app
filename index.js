003989698d187e167a87a72e3cfb9e3dedb0d896
Austin Bell <snarkeet@gmail.com>
2020-11-24 01:09:58 -0500

    filter states

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const form = document.querySelector("form");
 const textInput = document.querySelector("#text-input");
-const map = L.map("map").setView([37.0902, -95.7129], 4);
+const map = L.map("map").setView([37.0902, -95.7129], 3);
+const style = {
+    "color": "white",
+    "fillColor": "black",
+    "weight": 1,
+    "opacity": 0.25,
+    "dashArray": 0,
+    "fillOpacity": 0.65
+}
 const accessToken = "pk.eyJ1IjoieGF2aWVyYmVsbGhhZGRvbiIsImEiOiJja2h0dWJzd3owMnV0MnJydmI5dXp1MjJrIn0.XsZhwZnA3zq2_SZZ5TF1RA"
 
 L.tileLayer(
@@ -17,42 +25,45 @@ L.tileLayer(
   }
 ).addTo(map);
 
-const style = {
-    "color": "white",
-    "fillColor": "black",
-    "weight": 1,
-    "opacity": 0.25,
-    "dashArray": 0,
-    "fillOpacity": 0.65
-}
-
 L.geoJson(statesData, {
     style: style
 }).addTo(map);
 
-let url = "https://api.846policebrutality.com/api/incidents";
-const req = new XMLHttpRequest();
-req.open("GET", url);
-req.send();
-req.onload = function () {
-  const data = JSON.parse(req.responseText).data;
+function visualize() {
+  let url = "https://api.846policebrutality.com/api/incidents";
+  const req = new XMLHttpRequest();
+  req.open("GET", url);
+  req.send();
+  req.onload = function () {
+    const data = JSON.parse(req.responseText).data;
+    data.forEach(incident => {
+        const lat = incident.geocoding.lat;
+        const long = incident.geocoding.long;
+        L.circle([lat, long], {
+          color: 'red',
+          fillColor: '#f03',
+          fillOpacity: 0.05,
+          radius: 50000,
+          weight: 1
+      }).addTo(map);
+    })
+  };
+}
 
-  data.forEach(incident => {
-      const lat = incident.geocoding.lat;
-      const long = incident.geocoding.long;
+function handleSearch(searchTerm) {
+  let url = "https://api.846policebrutality.com/api/incidents?filter[state]=" + searchTerm.split(" ").join("+");
+  const req = new XMLHttpRequest();
+  req.open("GET", url);
+  req.send();
+  req.onload = function () {
+    const data = JSON.parse(req.responseText).data;
+    console.log(data)
+  }
+}
 
-      L.circle([lat, long], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.05,
-        radius: 50000,
-        weight: 1
-    }).addTo(map);
-  })
-  
-};
+visualize();
 
 form.addEventListener("submit", event => {
   event.preventDefault();
-  console.log(textInput.value)
+  handleSearch(textInput.value);
 })
