e93f13f9561ad459a0b6cef5f7aafaec74015d61
Austin Bell <snarkeet@gmail.com>
2021-01-11 22:10:25 -0800

    fixed to account for Android soft keyboard

--- index.js
+++ index.js
@@ -279,3 +279,10 @@ window.addEventListener("resize", (event) => {
     map.setZoom(3);
   }
 });
+
+let viewport = document.querySelector("meta[name=viewport]")
+ viewport.setAttribute("content", "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0")
+
+document.documentElement.style.setProperty("overflow", "auto")
+const metaViewport = document.querySelector("meta[name=viewport]")
+metaViewport.setAttribute("content", "height=" + initialHeight + "px, width=device-width, initial-scale=1.0")
